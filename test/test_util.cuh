8b5433f1bbc5ce568ecaf281ca1e2f4e949da79e
Patrick M. Jensen <patmjen@gmail.com>
2019-07-05 17:48:28 +0200

    Add constructor tests for volume types

--- test/test_util.cuh
+++ test/test_util.cuh
@@ -1,17 +1,17 @@
 #ifndef TEST_UTIL_CUH__
 #define TEST_UTIL_CUH__
 
-namespace gpho {
+#define ASSERT_CUDA_SUCCESS(expr) do { \
+	cudaError_t res__ = (expr); \
+	ASSERT_EQ(res__, cudaSuccess) << "CUDA error: " << cudaGetErrorString(res__); \
+} while(false)
 
-namespace test {
+#define EXPECT_CUDA_SUCCESS(expr) do { \
+	cudaError_t res__ = (expr); \
+	EXPECT_EQ(res__, cudaSuccess) << "CUDA error: " << cudaGetErrorString(res__); \
+} while (false)
 
 const int3 int3_0 = make_int3(0, 0, 0);
 const int3 int3_1 = make_int3(1, 1, 1);
-
-template <class Ty> void nonDeleter(Ty *x){};
-
-} // namespace test
-
-} // namespace gpho
 
 #endif // TEST_UTIL_CUH__
--- test/volume_tests.cu
+++ test/volume_tests.cu
@@ -9,115 +9,147 @@
 
 // For convenience
 using namespace gpho;
-using namespace gpho::test;
 
-TEST(VolumeTest, DefaultInit)
+template <class VolTy>
+class AllVolumesTest : public ::testing::Test {
+public:
+	using VolumeType = VolTy;
+};
+
+// TODO: Also parameterize on the type contained in the volumes
+using AllVolumeTypes = ::testing::Types<Volume<float>, DeviceVolume<float>, HostVolume<float>, PinnedVolume<float>>;
+TYPED_TEST_SUITE(AllVolumesTest, AllVolumeTypes);
+
+TYPED_TEST(AllVolumesTest, DefaultInit)
 {
-	Volume<float> vol;
+	typename TestFixture::VolumeType vol;
 	EXPECT_EQ(vol.data(), nullptr);
 	EXPECT_EQ(vol.size(), int3_0);
 }
 
-TEST(VolumeTest, ParamConstructor)
+template <class VolTy, class Ty>
+void verifyCopiedState(const VolTy& vol, const Ty *expectedPtr, const int3& expectedSize,
+	const std::shared_ptr<Ty>& outerSharedPtr, int expectedUseCount, std::string postfix = "")
+{
+	EXPECT_EQ(vol.data(), expectedPtr) << postfix;
+	EXPECT_EQ(vol.size(), expectedSize) << postfix;
+	EXPECT_EQ(outerSharedPtr.use_count(), expectedUseCount) << postfix;
+}
+
+TYPED_TEST(AllVolumesTest, CopySharedPtrConstructor)
 {
 	float x = 1.0f;
 	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
 	int3 size = int3_1;
 	ASSERT_EQ(xPtr.use_count(), 1) << "Test pre-condition";
 
-	// Copy construct shared_ptr
 	{
-		Volume<float> vol1(xPtr, size);
-		EXPECT_EQ(vol1.data(), &x);
-		EXPECT_EQ(vol1.size(), size);
-		EXPECT_EQ(xPtr.use_count(), 2);
+		typename TestFixture::VolumeType vol(xPtr, size);
+		verifyCopiedState(vol, &x, size, xPtr, 2);
 	}
-	ASSERT_EQ(xPtr.use_count(), 1) << "Copy post-condition";
-
-	// Move construct shared_ptr
-	std::shared_ptr<float> xPtrCopy(xPtr);
-	ASSERT_EQ(xPtrCopy.get(), xPtr.get()) << "Move pre-condition";
-	ASSERT_EQ(xPtr.use_count(), 2) << "Move pre-condition";
+	ASSERT_EQ(xPtr.use_count(), 1);
 
 	{
-		Volume<float> vol2(std::move(xPtrCopy), size);
-		EXPECT_EQ(vol2.data(), &x);
-		EXPECT_EQ(vol2.size(), size);
-
-		EXPECT_EQ(xPtrCopy.get(), nullptr);
-		EXPECT_EQ(xPtrCopy.use_count(), 0);
-
-		EXPECT_EQ(xPtr.use_count(), 2);
+		typename TestFixture::VolumeType vol(xPtr, size.x, size.y, size.z);
+		verifyCopiedState(vol, &x, size, xPtr, 2);
 	}
 	EXPECT_EQ(xPtr.use_count(), 1);
 }
 
-TEST(VolumeTest, Copy)
+TYPED_TEST(AllVolumesTest, Copy)
 {
 	float x = 1.0f;
 	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
-	Volume<float> vol1(xPtr, int3_1);
+	int3 size = int3_1;
+	typename TestFixture::VolumeType vol1(xPtr, size);
 	ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
 
 	// Copy construction
 	{
-		Volume<float> vol2(vol1);
-		EXPECT_EQ(vol2.data(), vol1.data()) << "Construction";
-		EXPECT_EQ(vol2.size(), vol1.size()) << "Construction";
-
-		EXPECT_EQ(xPtr.use_count(), 3) << "Construction";
+		typename TestFixture::VolumeType vol2(vol1);
+		verifyCopiedState(vol2, vol1.data(), size, xPtr, 3);
 	}
 	ASSERT_EQ(xPtr.use_count(), 2) << "Construction post-condition";
 
 	// Copy assignment
 	{
-		Volume<float> vol3 = vol1;
-		EXPECT_EQ(vol3.data(), vol1.data()) << "Assignment";
-		EXPECT_EQ(vol3.size(), vol1.size()) << "Assignment";
-
-		EXPECT_EQ(xPtr.use_count(), 3) << "Assignment";
+		typename TestFixture::VolumeType vol3 = vol1;
+		verifyCopiedState(vol3, vol1.data(), size, xPtr, 3);
 	}
 	EXPECT_EQ(xPtr.use_count(), 2) << "Assignment post-condition";
 }
 
-TEST(VolumeTest, MoveConstruction)
+TYPED_TEST(AllVolumesTest, MoveSharedPtrConstructor)
 {
 	float x = 1.0f;
 	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
 	int3 size = int3_1;
-	Volume<float> vol1(xPtr, size);
-	ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
+	ASSERT_EQ(xPtr.use_count(), 1) << "Test pre-condition";
+
+	auto verifyState = [&](const auto& vol, const auto& xPtrCopy) {
+		verifyCopiedState(vol, &x, size, xPtr, 2);
+
+		EXPECT_EQ(xPtrCopy.get(), nullptr);
+		EXPECT_EQ(xPtrCopy.use_count(), 0);
+	};
 
 	{
-		Volume<float> vol2(std::move(vol1));
-		EXPECT_EQ(vol2.data(), &x);
-		EXPECT_EQ(vol2.size(), size);
+		std::shared_ptr<float> xPtrCopy(xPtr);
+		ASSERT_EQ(xPtrCopy.get(), xPtr.get()) << "Test pre-condition";
+		ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
 
-		EXPECT_EQ(vol1.data(), nullptr);
-		EXPECT_EQ(vol1.size(), int3_0);
+		typename TestFixture::VolumeType vol(std::move(xPtrCopy), size);
+		verifyState(vol, xPtrCopy);
+	}
+	EXPECT_EQ(xPtr.use_count(), 1);
 
-		EXPECT_EQ(xPtr.use_count(), 2);
+	{
+		std::shared_ptr<float> xPtrCopy(xPtr);
+		ASSERT_EQ(xPtrCopy.get(), xPtr.get()) << "Test pre-condition";
+		ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
+
+		typename TestFixture::VolumeType vol(std::move(xPtrCopy), size.x, size.y, size.z);
+		verifyState(vol, xPtrCopy);
 	}
 	EXPECT_EQ(xPtr.use_count(), 1);
 }
 
-TEST(VolumeTest, MoveAssignment)
+template <class VolTy, class Ty>
+void verifyMovedState(const VolTy& vol1, const VolTy& vol2, const Ty *expectedPtr, const int3& expectedSize,
+	const std::shared_ptr<Ty>& outerSharedPtr, int expectedUseCount, std::string postfix = "")
+{
+	verifyCopiedState(vol2, expectedPtr, expectedSize, outerSharedPtr, expectedUseCount, postfix);
+
+	EXPECT_EQ(vol1.data(), nullptr);
+	EXPECT_EQ(vol1.size(), int3_0);
+}
+
+TYPED_TEST(AllVolumesTest, MoveConstruction)
 {
 	float x = 1.0f;
 	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
 	int3 size = int3_1;
-	Volume<float> vol1(xPtr, size);
+	typename TestFixture::VolumeType vol1(xPtr, size);
 	ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
 
 	{
-		Volume<float> vol2 = std::move(vol1);
-		EXPECT_EQ(vol2.data(), &x);
-		EXPECT_EQ(vol2.size(), size);
+		typename TestFixture::VolumeType vol2(std::move(vol1));
+		verifyMovedState(vol1, vol2, &x, size, xPtr, 2);
+	}
+	EXPECT_EQ(xPtr.use_count(), 1);
+}
 
-		EXPECT_EQ(vol1.data(), nullptr);
-		EXPECT_EQ(vol1.size(), int3_0);
+TYPED_TEST(AllVolumesTest, MoveAssignment)
+{
+	float x = 1.0f;
+	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
+	int3 size = int3_1;
+	typename TestFixture::VolumeType vol1(xPtr, size);
+	ASSERT_EQ(xPtr.use_count(), 2) << "Test pre-condition";
 
-		EXPECT_EQ(xPtr.use_count(), 2);
+	{
+		typename TestFixture::VolumeType vol2 = std::move(vol1);
+		verifyMovedState(vol1, vol2, &x, size, xPtr, 2);
 	}
 	EXPECT_EQ(xPtr.use_count(), 1);
 }
@@ -153,15 +185,129 @@ TEST(VolumeTest, Reshape)
 
 TEST(VolumeTest, Idx)
 {
+	// NOTE: The point of this test is **only** to verify that the output matches the idx function
 	int3 size = make_int3(2, 3, 5);
 	Volume<float> vol(std::shared_ptr<float>(), size);
 	ASSERT_EQ(vol.size(), size) << "Test pre-condition";
 
-	// NOTE: The point of this test is **only** to verify that the output matches the idx function
 	EXPECT_EQ(vol.idx(0, 0, 0), idx(0, 0, 0, vol.size()));
 	EXPECT_EQ(vol.idx(1, 0, 0), idx(1, 0, 0, vol.size()));
 	EXPECT_EQ(vol.idx(0, 1, 0), idx(0, 1, 0, vol.size()));
 	EXPECT_EQ(vol.idx(0, 0, 1), idx(0, 0, 1, vol.size()));
 	EXPECT_EQ(vol.idx(1, 1, 3), idx(1, 1, 3, vol.size()));
 	EXPECT_EQ(vol.idx(1, 2, 4), idx(1, 2, 4, vol.size()));
+}
+
+TEST(DeviceVolumeTest, Copy)
+{
+	// NOTE: We only test the parts we are specific to DeviceVolume
+	float x = 1.0f;
+	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
+	DeviceVolume<float> vol1(xPtr, int3_1);
+
+	auto verifyData = [&](const auto& vol1, const auto& vol2, std::string postfix) {
+		// We convert to a Volume reference so we actually get the shared_ptr pointer
+		const Volume<float>& v1 = vol1;
+		const Volume<float>& v2 = vol2;
+		EXPECT_EQ(v1.data(), v2.data());
+	};
+
+	// Copy construction
+	DeviceVolume<float> vol2(vol1);
+	verifyData(vol1, vol2, "Construction");
+
+	// Copy assignment
+	DeviceVolume<float> vol3 = vol1;
+	verifyData(vol1, vol3, "Assignment");
+}
+
+TEST(DeviceVolumeTest, Move)
+{
+	// NOTE: We only test the parts we are specific to DeviceVolume
+	float x = 1.0f;
+	std::shared_ptr<float> xPtr(&x, nonDeleter<float>);
+	int3 size = int3_1;
+	DeviceVolume<float> vol1(xPtr, size);
+	ASSERT_EQ(vol1.data(), &x);
+
+	auto verifyData = [&](const auto& vol, std::string postfix) {
+		const Volume<float>& v = vol;
+		ASSERT_EQ(v.data(), &x) << postfix;
+	};
+
+	DeviceVolume<float> vol2(std::move(vol1));
+	ASSERT_NO_FATAL_FAILURE(verifyData(vol2, "Construction"));
+
+	DeviceVolume<float> vol3 = std::move(vol2);
+	ASSERT_NO_FATAL_FAILURE(verifyData(vol3, "Assignment"));
+}
+
+template <class VolTy, class Ty>
+void verifyConstruction(const VolTy& vol, Ty *data, int3 size)
+{
+	::testing::StaticAssertTypeEq<typename VolTy::Type, Ty>();
+	EXPECT_EQ(vol.data(), data);
+	EXPECT_EQ(vol.size(), size);
+}
+
+TEST(DeviceVolumeTest, PtrConstructor)
+{
+	int3 size = int3_1;
+	float *x = nullptr;
+	ASSERT_CUDA_SUCCESS(cudaMalloc(&x, prod(size) * sizeof(*x)));
+
+	verifyConstruction(DeviceVolume<float>(x, size), x, size);
+	EXPECT_CUDA_SUCCESS(cudaGetLastError());
+
+	x = nullptr;
+	ASSERT_CUDA_SUCCESS(cudaMalloc(&x, prod(size) * sizeof(*x)));
+	verifyConstruction(DeviceVolume<float>(x, size.x, size.y, size.z), x, size);
+	EXPECT_CUDA_SUCCESS(cudaGetLastError());
+}
+
+TEST(HostVolumeTest, PtrConstructor)
+{
+	int3 size = int3_1;
+	float *x = nullptr;
+	ASSERT_NO_THROW(x = new float[prod(size)]);
+
+	verifyConstruction(HostVolume<float>(x, size), x, size);
+
+	ASSERT_NO_THROW(x = new float[prod(size)]);
+	verifyConstruction(HostVolume<float>(x, size.x, size.y, size.z), x, size);
+}
+
+TEST(HostVolumeTest, DifferentAllocator)
+{
+	int3 size = int3_1;
+	float *x = static_cast<float *>(malloc(prod(size) * sizeof(*x)));
+	ASSERT_NE(x, nullptr) << "Allocation failed";
+
+	// We don't really have any way to check if the call to free was successful, except to run the
+	// tests under something like valgrind. We can only check that it was called, and that it did
+	// not crash the program.
+	bool wasFreed = false;
+	{
+		auto deleter = [&](float *ptr) {
+			wasFreed = true;
+			free(ptr);
+		};
+		HostVolume<float> vol(x, size, deleter);
+	}
+	ASSERT_TRUE(wasFreed);
+}
+
+TEST(PinnedVolumeTest, PtrConstructor)
+{
+	float *x = nullptr;
+	int3 size = int3_1;
+	ASSERT_CUDA_SUCCESS(cudaMallocHost(&x, prod(size) * sizeof(*x)));
+
+	verifyConstruction(PinnedVolume<float>(x, size), x, size);
+	EXPECT_CUDA_SUCCESS(cudaGetLastError());
+
+	x = nullptr;
+	ASSERT_CUDA_SUCCESS(cudaMallocHost(&x, prod(size) * sizeof(*x)));
+	verifyConstruction(PinnedVolume<float>(x, size.x, size.y, size.z), x, size);
+	EXPECT_CUDA_SUCCESS(cudaGetLastError());
 }
