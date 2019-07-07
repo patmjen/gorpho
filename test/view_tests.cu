e382b8a83f8f5b6a4efefc69b63fcd499aa1e2ca
Patrick M. Jensen <patmjen@gmail.com>
2019-07-07 21:57:45 +0200

    Add some tests for view functionality

---
+++ test/view_tests.cu
@@ -0,0 +1,108 @@
+#include "gtest/gtest.h"
+#include "helper_math.cuh"
+
+#include "view.cuh"
+#include "volume.cuh"
+#include "test_util.cuh"
+
+// For convenience
+using namespace gpho;
+using namespace gpho::detail;
+
+TEST(SizedBaseTest, Numel)
+{
+	int3 size1 = make_int3(2, 3, 5);
+	SizedBase sb1(size1);
+	ASSERT_EQ(sb1.size(), size1) << "Test pre-condition";
+	EXPECT_EQ(sb1.numel(), 2 * 3 * 5);
+
+	int3 size2 = make_int3(2, 3, 0);
+	SizedBase sb2(size2);
+	ASSERT_EQ(sb2.size(), size2) << "Test pre-condition";
+	EXPECT_EQ(sb2.numel(), 0);
+}
+
+TEST(SizedBaseTest, Reshape)
+{
+	// TODO: Test device version
+	int3 size = make_int3(2, 4, 8);
+	SizedBase sb(size);
+	ASSERT_EQ(sb.size(), size) << "Test pre-condition";
+
+	int3 newSize = make_int3(16, 2, 2);
+	EXPECT_NO_THROW(sb.reshape(newSize));
+	ASSERT_EQ(sb.size(), newSize);
+
+	EXPECT_NO_THROW(sb.reshape(size.x, size.y, size.z));
+	EXPECT_EQ(sb.size(), size);
+
+	EXPECT_THROW(sb.reshape(1, 2, 3), std::length_error);
+}
+
+TEST(SizedBaseTest, Idx)
+{
+	// NOTE: The point of this test is **only** to verify that the output matches the idx function
+	int3 size = make_int3(2, 3, 5);
+	SizedBase sb(size);
+	ASSERT_EQ(sb.size(), size) << "Test pre-condition";
+
+	EXPECT_EQ(sb.idx(0, 0, 0), idx(0, 0, 0, sb.size()));
+	EXPECT_EQ(sb.idx(1, 0, 0), idx(1, 0, 0, sb.size()));
+	EXPECT_EQ(sb.idx(0, 1, 0), idx(0, 1, 0, sb.size()));
+	EXPECT_EQ(sb.idx(0, 0, 1), idx(0, 0, 1, sb.size()));
+	EXPECT_EQ(sb.idx(1, 1, 3), idx(1, 1, 3, sb.size()));
+	EXPECT_EQ(sb.idx(1, 2, 4), idx(1, 2, 4, sb.size()));
+}
+
+template <class Vw>
+class AllViewsTest : public ::testing::Test {
+public:
+	using View = Vw;
+};
+
+// TODO: Also parameterize on the type contained in the volumes
+using AllViewTypes = ::testing::Types<ViewBase<float>, DeviceView<float>, HostView<float>, PinnedView<float>>;
+TYPED_TEST_SUITE(AllViewsTest, AllViewTypes);
+
+TYPED_TEST(AllViewsTest, DefaultInit)
+{
+	typename TestFixture::View vw;
+	EXPECT_EQ(vw.data(), nullptr);
+	EXPECT_EQ(vw.size(), int3_0);
+}
+
+TYPED_TEST(AllViewsTest, PtrConstructor)
+{
+	float x = 1.4f;
+	int3 size = int3_1;
+	typename TestFixture::View vw(&x, size);
+	EXPECT_EQ(vw.data(), &x);
+	EXPECT_EQ(*vw.data(), x);
+	EXPECT_EQ(vw.size(), size);
+}
+
+TYPED_TEST(AllViewsTest, ModifyValue)
+{
+	float x = 1.4f;
+	float newX = 2.3f;
+	int3 size = int3_1;
+	typename TestFixture::View vw(&x, size);
+	ASSERT_EQ(*vw.data(), x);
+
+	*vw.data() = newX;
+	EXPECT_EQ(*vw.data(), newX);
+	EXPECT_EQ(x, newX);
+}
+
+TYPED_TEST(AllViewsTest, NonConstToConstView)
+{
+	float x = 1.2f;
+	float *p = &x;
+	typename TestFixture::View vw1(p, int3_1);
+	typename TestFixture::View::ConstView vw2(vw1);
+
+	EXPECT_EQ(vw2.data(), vw2.data());
+	EXPECT_EQ(vw2.size(), vw2.size());
+	EXPECT_EQ(*vw1.data(), x);
+	EXPECT_EQ(*vw2.data(), x);
+}
--- test/volume_tests.cu
+++ test/volume_tests.cu
@@ -4,6 +4,7 @@
 #include "gtest/gtest.h"
 #include "helper_math.cuh"
 
+#include "view.cuh"
 #include "volume.cuh"
 #include "test_util.cuh"
 
@@ -11,10 +12,10 @@
 using namespace gpho;
 using namespace gpho::detail;
 
-template <class VolTy>
+template <class Vol>
 class AllVolumesTest : public ::testing::Test {
 public:
-	using VolumeType = VolTy;
+	using Volume = Vol;
 };
 
 // TODO: Also parameterize on the type contained in the volumes
@@ -23,13 +24,13 @@ TYPED_TEST_SUITE(AllVolumesTest, AllVolumeTypes);
 
 TYPED_TEST(AllVolumesTest, DefaultInit)
 {
-	typename TestFixture::VolumeType vol;
+	typename TestFixture::Volume vol;
 	EXPECT_EQ(vol.data(), nullptr);
 	EXPECT_EQ(vol.size(), int3_0);
 }
 
-template <class VolTy, class Ty>
-void verifyCopiedState(const VolTy& vol, const Ty *expectedPtr, const int3& expectedSize,
+template <class Vol, class Ty>
+void verifyCopiedState(const Vol& vol, const Ty *expectedPtr, const int3& expectedSize,
 	int expectedUseCount, std::string postfix = "")
 {
 	EXPECT_EQ(vol.data(), expectedPtr) << postfix;
@@ -37,23 +38,23 @@ void verifyCopiedState(const VolTy& vol, const Ty *expectedPtr, const int3& expe
 	EXPECT_EQ(vol.useCount(), expectedUseCount) << postfix;
 }
 
-template <class VolTy>
-void performCopyTest(const VolTy& vol)
+template <class Vol>
+void performCopyTest(const Vol& vol)
 {
 	int initialUseCount = vol.useCount();
 	int3 size = vol.size();
-	const typename VolTy::Type *data_ptr = vol.data();
+	const typename Vol::Type *data_ptr = vol.data();
 
 	// Copy construction
 	{
-		VolTy vol2(vol);
+		Vol vol2(vol);
 		verifyCopiedState(vol2, data_ptr, size, initialUseCount + 1);
 	}
 	ASSERT_EQ(vol.useCount(), 1) << "Construction post-condition";
 
 	// Copy assignment
 	{
-		VolTy vol3 = vol;
+		Vol vol3 = vol;
 		verifyCopiedState(vol3, data_ptr, size, initialUseCount + 1);
 	}
 	EXPECT_EQ(vol.useCount(), 1) << "Assignment post-condition";
@@ -82,8 +83,8 @@ TEST(PinnedVolumeTest, Copy)
 	ASSERT_CUDA_SUCCESS(cudaGetLastError());
 }
 
-template <class VolTy, class Ty>
-void verifyMovedState(const VolTy& vol1, const VolTy& vol2, const Ty *expectedPtr, const int3& expectedSize,
+template <class Vol, class Ty>
+void verifyMovedState(const Vol& vol1, const Vol& vol2, const Ty *expectedPtr, const int3& expectedSize,
 	int expectedUseCount, std::string postfix = "")
 {
 	verifyCopiedState(vol2, expectedPtr, expectedSize, expectedUseCount, postfix);
@@ -92,15 +93,15 @@ void verifyMovedState(const VolTy& vol1, const VolTy& vol2, const Ty *expectedPt
 	EXPECT_EQ(vol1.size(), int3_0);
 }
 
-template <class VolTy>
-void performMoveConstructionTest(VolTy vol)
+template <class Vol>
+void performMoveConstructionTest(Vol vol)
 {
 	int initialUseCount = vol.useCount();
-	const typename VolTy::Type *dataPtr = vol.data();
+	const typename Vol::Type *dataPtr = vol.data();
 	int3 size = vol.size();
 
 	{
-		VolTy vol2(std::move(vol));
+		Vol vol2(std::move(vol));
 		verifyMovedState(vol, vol2, dataPtr, size, initialUseCount);
 	}
 	EXPECT_EQ(vol.useCount(), 0);
@@ -129,15 +130,15 @@ TEST(PinnedVolumeTest, MoveConstruction)
 	ASSERT_CUDA_SUCCESS(cudaGetLastError());
 }
 
-template <class VolTy>
-void performMoveAssignmentTest(VolTy vol)
+template <class Vol>
+void performMoveAssignmentTest(Vol vol)
 {
 	int initialUseCount = vol.useCount();
-	const typename VolTy::Type *dataPtr = vol.data();
+	const typename Vol::Type *dataPtr = vol.data();
 	int3 size = vol.size();
 
 	{
-		VolTy vol2 = std::move(vol);
+		Vol vol2 = std::move(vol);
 		verifyMovedState(vol, vol2, dataPtr, size, initialUseCount);
 	}
 	EXPECT_EQ(vol.useCount(), 0);
@@ -166,55 +167,53 @@ TEST(PinnedVolumeTest, MoveAssignment)
 	ASSERT_CUDA_SUCCESS(cudaGetLastError());
 }
 
-
-template <class VolTy, class Ty>
-void verifyConstruction(const VolTy& vol, Ty *data, int3 size)
+TEST(DeviceVolumeTest, HostTransfer)
 {
-	EXPECT_EQ(vol.data(), data);
-	EXPECT_EQ(vol.size(), size);
+	float expected = 2.3f;
+	DeviceVolume<float> dvol = makeDeviceVolume<float>(1, 1, 1);
+	ASSERT_NO_FATAL_FAILURE(setDevicePtr(dvol.data(), expected));
+	ASSERT_CUDA_EQ(dvol.data(), expected); // TODO: Write separate tests for setDevicePtr
+
+	HostVolume<float> vol;
+	ASSERT_NO_THROW(vol = dvol.copyToHost());
+	EXPECT_EQ(*vol.data(), expected);
 }
 
-TEST(SizedBaseTest, Numel)
+TEST(HostVolumeTest, DeviceTransfer)
 {
-	int3 size1 = make_int3(2, 3, 5);
-	SizedBase sb1(size1);
-	ASSERT_EQ(sb1.size(), size1) << "Test pre-condition";
-	EXPECT_EQ(sb1.numel(), 2 * 3 * 5);
-
-	int3 size2 = make_int3(2, 3, 0);
-	SizedBase sb2(size2);
-	ASSERT_EQ(sb2.size(), size2) << "Test pre-condition";
-	EXPECT_EQ(sb2.numel(), 0);
+	float expected = 2.3f;
+	HostVolume<float> vol = makeHostVolume<float>(1, 1, 1);
+	*vol.data() = expected;
+	ASSERT_EQ(*vol.data(), expected);
+	
+	DeviceVolume<float> dvol;
+	ASSERT_NO_THROW(dvol = vol.copyToDevice());
+	EXPECT_CUDA_EQ(dvol.data(), expected);
 }
 
-TEST(SizedBaseTest, Reshape)
+template <class Vol>
+class AllDerivedVolumesTest : public ::testing::Test {
+public:
+	using Volume = Vol;
+};
+
+// TODO: Also parameterize on the type contained in the volumes
+using AllDerivedVolumeTypes = ::testing::Types<DeviceVolume<float>, HostVolume<float>, PinnedVolume<float>>;
+TYPED_TEST_SUITE(AllDerivedVolumesTest, AllDerivedVolumeTypes);
+
+TYPED_TEST(AllDerivedVolumesTest, GetView)
 {
-	// TODO: Test device version
-	int3 size = make_int3(2, 4, 8);
-	SizedBase sb(size);
-	ASSERT_EQ(sb.size(), size) << "Test pre-condition";
+	using Volume = typename TestFixture::Volume;
+	using Type = typename Volume::Type;
+	Volume vol;
+	ASSERT_NO_THROW(vol = makeVolume<Volume>(int3_1));
+	auto vw = vol.view();
 
-	int3 newSize = make_int3(16, 2, 2);
-	EXPECT_NO_THROW(sb.reshape(newSize));
-	ASSERT_EQ(sb.size(), newSize);
+	EXPECT_EQ(vw.data(), vol.data());
+	EXPECT_EQ(vw.size(), vol.size());
 
-	EXPECT_NO_THROW(sb.reshape(size.x, size.y, size.z));
-	EXPECT_EQ(sb.size(), size);
-
-	EXPECT_THROW(sb.reshape(1, 2, 3), std::length_error);
-}
-
-TEST(SizedBaseTest, Idx)
-{
-	// NOTE: The point of this test is **only** to verify that the output matches the idx function
-	int3 size = make_int3(2, 3, 5);
-	SizedBase sb(size);
-	ASSERT_EQ(sb.size(), size) << "Test pre-condition";
-
-	EXPECT_EQ(sb.idx(0, 0, 0), idx(0, 0, 0, sb.size()));
-	EXPECT_EQ(sb.idx(1, 0, 0), idx(1, 0, 0, sb.size()));
-	EXPECT_EQ(sb.idx(0, 1, 0), idx(0, 1, 0, sb.size()));
-	EXPECT_EQ(sb.idx(0, 0, 1), idx(0, 0, 1, sb.size()));
-	EXPECT_EQ(sb.idx(1, 1, 3), idx(1, 1, 3, sb.size()));
-	EXPECT_EQ(sb.idx(1, 2, 4), idx(1, 2, 4, sb.size()));
+	// Make sure a const reference returns a const view
+	const Volume& constRef = vol;
+	auto cvw = constRef.view();
+	::testing::StaticAssertTypeEq<decltype(cvw)::Type, const Type>();
 }
